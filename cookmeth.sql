1ed244fb97558090053bf1d0e47239b73581deea
MaitreyaGakure <106176369+MaitreyaGakure@users.noreply.github.com>
2022-06-08 08:27:06 -0500

    alterações p/criar php banco

---
+++ cookmeth.sql
@@ -0,0 +1,42 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 08-Jun-2022 às 15:22
+-- Versão do servidor: 10.4.11-MariaDB
+-- versão do PHP: 7.4.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `heisenberg`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cookmeth`
+--
+
+CREATE TABLE `cookmeth` (
+  `Nome` varchar(255) NOT NULL,
+  `Personagem` varchar(255) NOT NULL,
+  `Pollos Hermanos` varchar(1) NOT NULL,
+  `Meth Cook` varchar(1) NOT NULL,
+  `DEA` varchar(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
